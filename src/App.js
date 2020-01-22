45f195a9cd74393cf2e3f032c604579c686c5ae7
junaid7lone <junaid7lone@gmail.com>
2020-01-22 23:46:59 +0530

    converted fromat from 24 to 12 hr

--- src/App.js
+++ src/App.js
@@ -1,138 +1,175 @@
-import React from 'react';
-import './App.css';
-import Clock from 'react-live-clock';
-import { Container, Row, Col } from 'reactstrap';
-import Fajr  from "./images/Fajr.svg";
-import Dhuhr  from "./images/Dhuhr.svg";
-import Asr  from "./images/Asr.svg";
-import Maghrib  from "./images/Maghrib.svg";
-import Isha  from "./images/Isha.svg";
+import React from "react";
+import "./App.css";
+import Clock from "react-live-clock";
+import { Container, Row, Col } from "reactstrap";
+import Fajr from "./images/Fajr.svg";
+import Dhuhr from "./images/Dhuhr.svg";
+import Asr from "./images/Asr.svg";
+import Maghrib from "./images/Maghrib.svg";
+import Isha from "./images/Isha.svg";
 import azan from "./sound/azan.mp3";
 import _ from "lodash";
-
+import { fetch as fetchPolyfill } from "whatwg-fetch";
 
 const prayers = {
-  Fajr: {label:"Fajr", time:"", color:"#753a16", icon : Fajr, urduName:"فجر"},
-  Dhuhr: {label:"Dhuhr", time:"", color:"#8dc1e8",icon: Dhuhr, urduName:"ظهر"},
-  Asr: {label:"Asr", time:"",color:"#0097da", icon: Asr, urduName:"عصر"},
-  Maghrib: {label:"Maghrib", time:"",color:"#d3503b",icon:Maghrib, urduName:"مغرب"},
-  Isha: {label:"Isha", time:"",color:"#293a69",icon: Isha, urduName:"عشاء"},
-}
+  Fajr: {
+    label: "Fajr",
+    time: "",
+    color: "#753a16",
+    icon: Fajr,
+    urduName: "فجر"
+  },
+  Dhuhr: {
+    label: "Dhuhr",
+    time: "",
+    color: "#8dc1e8",
+    icon: Dhuhr,
+    urduName: "ظهر"
+  },
+  Asr: { label: "Asr", time: "", color: "#0097da", icon: Asr, urduName: "عصر" },
+  Maghrib: {
+    label: "Maghrib",
+    time: "",
+    color: "#d3503b",
+    icon: Maghrib,
+    urduName: "مغرب"
+  },
+  Isha: {
+    label: "Isha",
+    time: "",
+    color: "#293a69",
+    icon: Isha,
+    urduName: "عشاء"
+  }
+};
 
+const url = "https://api.pray.zone/v2/times/today.json?city=karachi";
 
-const url = "https://api.pray.zone/v2/times/today.json?city=karachi"
+class App extends React.Component {
+  state = { prayers };
 
-
-class  App extends React.Component{
-  state = {prayers}
-
-  componentDidMount(){
+  componentDidMount() {
     this.keepDisplayOn();
     this.getPrayerTime();
   }
-  
-  playSound(){
-    const sound = new Audio(azan)
+
+  playSound() {
+    const sound = new Audio(azan);
     sound.play();
   }
 
-  get allTimes(){
-    const times = _.map(this.state.prayers, function(prayer){return prayer.time});
-    return times; 
+  get allTimes() {
+    const times = _.map(this.state.prayers, function(prayer) {
+      return prayer.time;
+    });
+    return times;
   }
 
-  initiateAlarmChecker(){
-    const everyMinute = 1000*60;
+  initiateAlarmChecker() {
+    const everyMinute = 1000 * 60;
     const allTimes = this.allTimes;
-    
-    setInterval(()=>{
-      const currentTime = new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: false })
-      if(_.includes(allTimes, currentTime)){
-        this.playSound()
-      }                                      
-    }, everyMinute)
 
+    setInterval(() => {
+      const currentTime = new Date().toLocaleString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: false
+      });
+      if (_.includes(allTimes, currentTime)) {
+        this.playSound();
+      }
+    }, everyMinute);
   }
 
-  keepDisplayOn(){
-    var Util={};
+  keepDisplayOn() {
+    var Util = {};
     Util.base64 = function(mimeType, base64) {
-      return 'data:' + mimeType + ';base64,' + base64;
+      return "data:" + mimeType + ";base64," + base64;
     };
 
-    var video = document.createElement('video');
-      video.setAttribute('loop', '');
+    var video = document.createElement("video");
+    video.setAttribute("loop", "");
 
-      function addSourceToVideo(element, type, dataURI) {
-        var source = document.createElement('source');
-        source.src = dataURI;
-        source.type = 'video/' + type;
-        element.appendChild(source);
-      }
+    function addSourceToVideo(element, type, dataURI) {
+      var source = document.createElement("source");
+      source.src = dataURI;
+      source.type = "video/" + type;
+      element.appendChild(source);
+    }
 
-      addSourceToVideo(video,'webm', Util.base64('video/webm', 'GkXfo0AgQoaBAUL3gQFC8oEEQvOBCEKCQAR3ZWJtQoeBAkKFgQIYU4BnQI0VSalmQCgq17FAAw9CQE2AQAZ3aGFtbXlXQUAGd2hhbW15RIlACECPQAAAAAAAFlSua0AxrkAu14EBY8WBAZyBACK1nEADdW5khkAFVl9WUDglhohAA1ZQOIOBAeBABrCBCLqBCB9DtnVAIueBAKNAHIEAAIAwAQCdASoIAAgAAUAmJaQAA3AA/vz0AAA='));
-      addSourceToVideo(video, 'mp4', Util.base64('video/mp4', 'AAAAHGZ0eXBpc29tAAACAGlzb21pc28ybXA0MQAAAAhmcmVlAAAAG21kYXQAAAGzABAHAAABthADAowdbb9/AAAC6W1vb3YAAABsbXZoZAAAAAB8JbCAfCWwgAAAA+gAAAAAAAEAAAEAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAIVdHJhawAAAFx0a2hkAAAAD3wlsIB8JbCAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAQAAAAAAIAAAACAAAAAABsW1kaWEAAAAgbWRoZAAAAAB8JbCAfCWwgAAAA+gAAAAAVcQAAAAAAC1oZGxyAAAAAAAAAAB2aWRlAAAAAAAAAAAAAAAAVmlkZW9IYW5kbGVyAAAAAVxtaW5mAAAAFHZtaGQAAAABAAAAAAAAAAAAAAAkZGluZgAAABxkcmVmAAAAAAAAAAEAAAAMdXJsIAAAAAEAAAEcc3RibAAAALhzdHNkAAAAAAAAAAEAAACobXA0dgAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAIAAgASAAAAEgAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABj//wAAAFJlc2RzAAAAAANEAAEABDwgEQAAAAADDUAAAAAABS0AAAGwAQAAAbWJEwAAAQAAAAEgAMSNiB9FAEQBFGMAAAGyTGF2YzUyLjg3LjQGAQIAAAAYc3R0cwAAAAAAAAABAAAAAQAAAAAAAAAcc3RzYwAAAAAAAAABAAAAAQAAAAEAAAABAAAAFHN0c3oAAAAAAAAAEwAAAAEAAAAUc3RjbwAAAAAAAAABAAAALAAAAGB1ZHRhAAAAWG1ldGEAAAAAAAAAIWhkbHIAAAAAAAAAAG1kaXJhcHBsAAAAAAAAAAAAAAAAK2lsc3QAAAAjqXRvbwAAABtkYXRhAAAAAQAAAABMYXZmNTIuNzguMw=='));
+    addSourceToVideo(
+      video,
+      "webm",
+      Util.base64(
+        "video/webm",
+        "GkXfo0AgQoaBAUL3gQFC8oEEQvOBCEKCQAR3ZWJtQoeBAkKFgQIYU4BnQI0VSalmQCgq17FAAw9CQE2AQAZ3aGFtbXlXQUAGd2hhbW15RIlACECPQAAAAAAAFlSua0AxrkAu14EBY8WBAZyBACK1nEADdW5khkAFVl9WUDglhohAA1ZQOIOBAeBABrCBCLqBCB9DtnVAIueBAKNAHIEAAIAwAQCdASoIAAgAAUAmJaQAA3AA/vz0AAA="
+      )
+    );
+    addSourceToVideo(
+      video,
+      "mp4",
+      Util.base64(
+        "video/mp4",
+        "AAAAHGZ0eXBpc29tAAACAGlzb21pc28ybXA0MQAAAAhmcmVlAAAAG21kYXQAAAGzABAHAAABthADAowdbb9/AAAC6W1vb3YAAABsbXZoZAAAAAB8JbCAfCWwgAAAA+gAAAAAAAEAAAEAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAIVdHJhawAAAFx0a2hkAAAAD3wlsIB8JbCAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAQAAAAAAIAAAACAAAAAABsW1kaWEAAAAgbWRoZAAAAAB8JbCAfCWwgAAAA+gAAAAAVcQAAAAAAC1oZGxyAAAAAAAAAAB2aWRlAAAAAAAAAAAAAAAAVmlkZW9IYW5kbGVyAAAAAVxtaW5mAAAAFHZtaGQAAAABAAAAAAAAAAAAAAAkZGluZgAAABxkcmVmAAAAAAAAAAEAAAAMdXJsIAAAAAEAAAEcc3RibAAAALhzdHNkAAAAAAAAAAEAAACobXA0dgAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAIAAgASAAAAEgAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABj//wAAAFJlc2RzAAAAAANEAAEABDwgEQAAAAADDUAAAAAABS0AAAGwAQAAAbWJEwAAAQAAAAEgAMSNiB9FAEQBFGMAAAGyTGF2YzUyLjg3LjQGAQIAAAAYc3R0cwAAAAAAAAABAAAAAQAAAAAAAAAcc3RzYwAAAAAAAAABAAAAAQAAAAEAAAABAAAAFHN0c3oAAAAAAAAAEwAAAAEAAAAUc3RjbwAAAAAAAAABAAAALAAAAGB1ZHRhAAAAWG1ldGEAAAAAAAAAIWhkbHIAAAAAAAAAAG1kaXJhcHBsAAAAAAAAAAAAAAAAK2lsc3QAAAAjqXRvbwAAABtkYXRhAAAAAQAAAABMYXZmNTIuNzguMw=="
+      )
+    );
 
     video.play();
   }
 
-  getPrayerTime(){
+  getPrayerTime() {
     fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw Error(response.statusText);
-      } else {
-        response.json().then(data => {
-          this.updatePrayerData(data)
-          this.initiateAlarmChecker();
-        });
-      }
-    })
-    .catch(err => {
-      console.log(err);
-      throw Error(err);
-    });
-  }
-
-  updatePrayerData(data){
-    const times = _.get(data,"results.datetime[0].times",null)
-    if(times){
-      _.map(Object.keys(times),item=>{
-        if(prayers[item]){
-          prayers[item].time = times[item]
+      .then(response => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        } else {
+          response.json().then(data => {
+            this.updatePrayerData(data);
+            this.initiateAlarmChecker();
+          });
         }
       })
-    }
-    this.setState({prayers})
+      .catch(err => {
+        console.log(err);
+        throw Error(err);
+      });
   }
-  
-  render(){
+
+  updatePrayerData(data) {
+    const times = _.get(data, "results.datetime[0].times", null);
+    if (times) {
+      _.map(Object.keys(times), item => {
+        if (prayers[item]) {
+          prayers[item].time = times[item];
+        }
+      });
+    }
+    this.setState({ prayers });
+  }
+
+  render() {
     return (
       <div className="App">
         <div className="header">
-          <div className="date">  
-            <Clock  
-              date={Date.now()} 
-              format={'ddd YYYY MM'}
-            /><br/>
-          </div>      
-          <Clock 
-            format={'h:mm:ss A'}
-            ticking={true} 
-            timezone={Intl.DateTimeFormat().resolvedOptions().timeZone}
-          />
+          <div className="date">
+            <Clock date={Date.now()} format={"ddd YYYY MM"} />
+            <br />
+          </div>
+          <Clock format={"h:mm:ss A"} ticking={true} />
         </div>
 
         <Container className="prayers-list">
-          {_.map(this.state.prayers,item=>{
-            return <Card 
-              key={item.label} 
-              name={item.label} 
-              icon={item.icon} 
-              color={item.color} 
-              urduName={item.urduName} 
-              time={item.time}
-            />
+          {_.map(this.state.prayers, item => {
+            return (
+              <Card
+                key={item.label}
+                name={item.label}
+                icon={item.icon}
+                color={item.color}
+                urduName={item.urduName}
+                time={item.time}
+              />
+            );
           })}
         </Container>
       </div>
@@ -140,18 +177,40 @@ class  App extends React.Component{
   }
 }
 
-
 const Card = props => {
-  return <Row className={`prayer-card-item ${props.name} `} >
+  return (
+    <Row className={`prayer-card-item ${props.name} `}>
       <Col className="name">
-        <img src={props.icon} className="icon" style={{color:props.color}}/> 
+        <img src={props.icon} className="icon" style={{ color: props.color }} />
         {props.name}
       </Col>
 
-      <Col className="time">{props.time}</Col>
-      
+      <Col className="time">
+        <TimeConversion time={props.time} />{" "}
+      </Col>
+
       <Col className="urdu">{props.urduName}</Col>
-  </Row>
-}
+    </Row>
+  );
+};
+
+const TimeConversion = props => {
+  let time = props.time;
+  let hour = Number(time.split(":")[0]);
+  let minute = Number(time.split(":")[1]);
+  let amPm = "am";
+  if (hour > 12) {
+    amPm = "pm";
+    hour = hour - 12;
+  } else if (hour === 12) {
+    amPm = "pm";
+  }
+
+  return (
+    <span>
+      {hour}:{minute} {amPm}
+    </span>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,54 @@
+import 'react-app-polyfill/stable';
+import 'whatwg-fetch'; 
+
+
+
+//import 'core-js/es';
+
+
+import "core-js";
+// polyfill only stable `core-js` features - ES and web standards:
+import "core-js/stable";
+// polyfill only stable ES features:
+import "core-js/es";
+
+// if you want to polyfill `Set`:
+// all `Set`-related features, with ES proposals:
+import "core-js/features/set";
+// stable required for `Set` ES features and features from web standards
+// (DOM collections iterator in this case):
+import "core-js/stable/set";
+// only stable ES features required for `Set`:
+import "core-js/es/set";
+
+
+// the same without global namespace pollution:
+// import Set from "core-js-pure/features/set";
+// import Set from "core-js-pure/stable/set";
+// import Set from "core-js-pure/es/set";
+
+// if you want to polyfill just required methods:
+import "core-js/features/set/intersection";
+import "core-js/stable/queue-microtask";
+import "core-js/es/array/from";
+
+// polyfill reflect metadata proposal:
+import "core-js/proposals/reflect-metadata";
+// polyfill all stage 2+ proposals:
+import "core-js/stage/2";
+import 'raf/polyfill';
+
+
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
